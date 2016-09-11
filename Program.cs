88ea41fa4bf95497965cfd74b5aaa2a4ff8c9951
xshotD <hi@youneeda.pw>
2016-09-11 09:14:56 -0500

    Uploading source.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace MyP
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine ("< MyP /> (c) S Stewart 2016");
+			Console.WriteLine ("Contacting ipify API - please wait");
+			WebClient webClient = new WebClient();
+			string MyP = webClient.DownloadString("https://api.ipify.org");
+			Console.WriteLine("IP is: {0}", MyP);
+
+
+		}
+	}
+} 
